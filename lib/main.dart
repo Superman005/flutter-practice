93e421ecc7f285ec303cec7db97891349ab55405
Superman005 <rohilraaaaz@gmail.com>
2021-05-16 17:45:51 +0545

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:practice4/home.dart';
+import 'package:practice4/pages/home_pages.dart';
+import 'package:practice4/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        // "/": (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+          child: Text(
+        'Login Page goes here',
+        style: TextStyle(color: Colors.red, fontSize: 20),
+      )),
+    );
+  }
+}
